70a3ce8c383f3c65cd62c1a81fe48d2c67168898
Harry Miller <harry@refersion.com>
2018-02-26 20:13:25 -0500

    usage

---
+++ todos.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+usage() {
+	cat <<EOF
+
+Usage: todos -l	: list todos.
+       todos -h : show this.
+       todos -o : open todos in editor.
+       todos -n : add a new todo.
+
+EOF
+}
+
+
+if [[ $# = 0 ]]; then
+	usage
+fi
+
+# Main.
+for option in "$@"; do
+	case $option in
+
+		-h | --h | --help)
+			usage
+			exit 0
+		;;
+
+	esac
+done
