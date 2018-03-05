3b60cc5b73b0a5159feb78199c5b73ac1eaa4069
Harry Miller <harry@refersion.com>
2018-03-05 15:20:31 -0500

    ability to make new todos

--- todos.sh
+++ todos.sh
@@ -15,6 +15,7 @@ usage() {
 usage: todos [options]
  	-h : show this.
 	-o : natively open todos dir.
+	-n : create a new todo
 EOF
 }
 
@@ -24,6 +25,33 @@ if [[ $# = 0 ]]; then
 	exit 0
 fi
 
+join_by() {
+	local IFS="$1"
+	shift
+	echo "$*"
+}
+
+make_title() {
+	echo $(date +%F)-$(shift; join_by - "$@")
+}
+
+validate_title() {
+	# Alphanumeric incl. dashes, 61 char max.
+	[[ "$@" =~ ^[a-zA-Z0-9][-a-zA-Z0-9]{0,61}[a-zA-Z0-9]$ ]]
+}
+
+# @param $1 : name of file
+new_todo() {
+	# make_title defined in util.
+	__TITLE=$(make_title $@)
+
+	validate_title $__TITLE || {
+		echo bad
+		exit 1;
+	}
+
+	(touch "$__DATA/$__TITLE" && cat) >> "$__DATA/$__TITLE"
+}
 
 # Main.
 for option in "$@"; do
@@ -39,5 +67,9 @@ for option in "$@"; do
 			exit 0
 		;;
 
+		-n | --new)
+			new_todo "$@"
+			exit 0
+		;;
 	esac
 done
